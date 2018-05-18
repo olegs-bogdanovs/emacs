b3274623b4c022fbe14b1572e6509aea4b5db145
olegs-bogdanovs <bogdanovsolegs@gmail.com>
2018-05-18 14:12:13 +0300

    Create init.el

---
+++ init.el
@@ -0,0 +1,21 @@
+; start package.el with emacs
+(require 'package)
+; add MELPA to repository list
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+; initialize package.el
+(package-initialize)
+; start auto-complete with emacs
+(require 'auto-complete)
+; do default config for auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+; start yasnippet with emacs
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(load-theme 'manoj-dark t)
+
+
+; enable neotree foe emacs
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
