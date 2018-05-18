5cf117b4caa1b71e941c573cafff752db9d420df
olegs-bogdanovs <bogdanovsolegs@gmail.com>
2018-05-18 16:36:18 +0300

    Update init.el

--- init.el
+++ init.el
@@ -12,10 +12,32 @@
 ; start yasnippet with emacs
 (require 'yasnippet)
 (yas-global-mode 1)
-
+; loads color theme manoj-dark
 (load-theme 'manoj-dark t)
-
-
 ; enable neotree foe emacs
 (require 'neotree)
+
+;;  magit configuration
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; disable backup files
+(setq make-backup-files nil)
+
+
+
 (global-set-key [f8] 'neotree-toggle)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit yasnippet-snippets neotree auto-complete-c-headers))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
